fc397939e57b9d73d6f8eafe5b2309bc5fe1c12a
Felipe <mattesfelipe@gmail.com>
2023-02-02 18:04:44 -0300

    Compiler parameters, obs: the build_std feature is only in nightly channel of rust

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,16 @@
 use core::panic::PanicInfo;
 
 
-// Overwriting the entry , it will be called by OS
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    loop {}
-}
-
 /// Its called when a panic occours
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+
+// Overwriting the entry , it will be called by OS
+// _start() call, are the same of Linux entry call (just for reference)
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
     loop {}
 }
