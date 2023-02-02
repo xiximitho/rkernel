27b350427781ccc60d44b12cd29dcd1db7994849
Felipe <mattesfelipe@gmail.com>
2023-02-02 17:39:46 -0300

    Panic handler and entry_point

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+
+// Overwriting the entry , it will be called by OS
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+/// Its called when a panic occours
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
