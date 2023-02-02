3467794741cb72f49b9cf4ead08dca2fee659917
Felipe <mattesfelipe@gmail.com>
2023-02-02 18:57:14 -0300

    Dependencies to make the bootable bin

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,20 @@ fn panic(_info: &PanicInfo) -> ! {
 }
 
 
+
+static HELLO: &[u8] = b"Um simples kernel";
+
 // Overwriting the entry , it will be called by OS
 // _start() call, are the same of Linux entry call (just for reference)
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
     loop {}
 }
